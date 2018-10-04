b368522eba82d2105e7c9fa14210d3eb23f9a3ad
Kristoffer Lind <kristoffer.lind@tretton37.se>
2018-10-04 15:42:00 +0200

    display selected dimension entries

---
+++ src/dimensionEntry.js
@@ -0,0 +1,70 @@
+import React, { Component, PropTypes } from 'react';
+// import PropTypes from 'prop-types';
+
+class DimensionEntry extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isSelected: false
+    };
+
+    // TODO: bind using class properties instead (requires babel plugin)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    const {
+      dimNo,
+      dimensionIndex
+    } = this.props;
+
+    this.setState({
+      isSelected: !this.state.isSelected
+    });
+    this.props.onToggle(dimNo, dimensionIndex);
+  }
+
+  render () {
+    const {
+      children,
+      showAs,
+      style,
+      label,
+      divideBy,
+      dindex,
+      divideByNumber
+    } = this.props;
+    const { isSelected } = this.state;
+
+    const isSelectedClass = isSelected ? ' is-selected' : '';
+
+    return (
+      <div className={`ui ${showAs}${isSelectedClass}`} style={style}>
+        {label.isHidden ? null : <a className={`ui ${label.size} ${label.orientation} ${label.alignment} label`} onClick={this.handleClick}>{label.text}</a>}
+        <div className={`ui ${divideBy} statistics`}>
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+DimensionEntry.propTypes = {
+  children: PropTypes.node.isRequired,
+  dimensionIndex: PropTypes.number,
+  dimNo: PropTypes.number,
+  dindex: PropTypes.any,
+  divideBy: PropTypes.any,
+  divideByNumber: PropTypes.any,
+  label: PropTypes.shape({
+    isHidden: PropTypes.bool,
+    orientation: PropTypes.string,
+    alignment: PropTypes.string,
+    size: PropTypes.any,
+    text: PropTypes.string
+  }).isRequired,
+  showAs: PropTypes.string.isRequired,
+  style: PropTypes.object
+};
+
+export default DimensionEntry;
