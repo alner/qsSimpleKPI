916cc44d9a2beb283531005ecb4170a15eb92041
Martin Walter <martin.walter@tretton37.com>
2018-10-04 10:23:46 +0200

    Refactoring of version logic

---
+++ scripts/get-latest-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit
+
+curl --silent "https://api.github.com/repos/$1/$2/releases/latest" | # Get latest release from GitHub api
+  grep '"tag_name":' |                                            # Get tag line
+  sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+
+
+### Inspired by https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+# Usage
+# $ get-latest-version.sh qlik-oss qsSimpleKPI
+# 0.12.0
