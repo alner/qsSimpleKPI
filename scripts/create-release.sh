84398d7f767d732f93807be9e4bb05d5db83ed9b
Martin Walter <martin.walter@tretton37.com>
2018-10-04 14:13:47 +0200

    Fixing output-directory and default value

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -4,7 +4,7 @@ set -o errexit
 echo "Bumping version from: $(scripts/print-version.sh $1 $2)"
 VERSION="$(scripts/bump-version.sh $1 $2)"
 echo "to: $VERSION"
-$HOME/bin/ghr -t ${ghoauth} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${VERSION} "./deploy/$2.zip"
+$HOME/bin/ghr -t ${ghoauth} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${VERSION} "./build/$2.zip"
 
 
 # Usage
