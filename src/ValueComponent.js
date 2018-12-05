033c4cdcf132dca31387c6c7993cc02360222b37
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2018-12-05 11:07:35 +0100

    disable user interaction with master visualization

--- src/ValueComponent.js
+++ src/ValueComponent.js
@@ -67,7 +67,7 @@ let DragDropSpec = {
     //let height = $(placeElement).height();
     if(itemid && itemid.trim().length > 0) {
       ReactDOM.unmountComponentAtNode(placeElement);
-      qlik.currApp().getObject(placeElement, itemid).then((object) => {
+      qlik.currApp().getObject(placeElement, itemid, { noInteraction: true }).then((object) => {
         //let newHeight = height - QLIK_COMP_TOOLBAR_HEIGHT;
         self.onObjectInvalidated = function(){
           if(object.layout.qSelectionInfo.qInSelections) {
